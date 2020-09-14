3b720e8a9013519d9d1f935f71716c32eceb82d6
Audu Martin <audumart@gmail.com>
2020-09-14 09:39:19 +0100

    further adjustments

--- app/server/models/users.js
+++ app/server/models/users.js
@@ -26,32 +26,69 @@ function id(){
 }
 
 
-    let Martin = new User(id(), 'Martin', 'Audu', 'audumart@gmail.com', 'martinchillout', '16/0013', 'Computer Science', '2020');
+
+
+
    
 
    
 class Users extends DataModel {
     authenticate(email, password) {
-        if (this.email == email && this.password == password) {
-            return true;
+        var user = this.getByEmail(email);
+
+        if (user != null) {
+            if (user.password === password) {
+                return true;
+            }
+            return false;
         }
-        return null;
+        return false;
     }
 
     getByEmail(email) {
-    
+        for (var i = 0; i < this.data.length; i++) {
+            if (this.data[i].email == email) {
+                return this.data[i];
 
+            }
+        }
 
+        return null;
     }
 
     getByMatricNumber(matricNumber) {
+        for (var i = 0; i < this.data.length; i++) {
+            if (this.data[i].matricNumber == matricNumber) {
+                return this.data[i];
+            }
+        }
 
+        return null;
     }
 
     validate(obj) {
+        if (obj.firstname.length == 0 || obj.lastname.length == 0 || obj.email.length == 0 || obj.password.length == 0 || obj.matricNumber.length == 0 || obj.program.length == 0 || obj.graduationYear.length == 0) {
+            return false;
+        }
+        for (var i = 0; i < this.data.length; i++) {
+            if (this.data[i].email === obj.email) {
+                return false;
+            }
+        }
+        for (var i = 0; i < this.data.length; i++) {
+            if (this.data[i].matricNumber === obj.matricNumber) {
+                return false;
+            }
+        }
+        if (obj.password.length < 7) {
+            return false;
+        }
 
+        return true;
     }
 }
+
+   
 
 // Do not worry about the below for now; It is included so that we can test your code
 // We will cover module exports in later parts of this course
